3838d93913026f1d378e3f62e10c261d372e7088
Vladyslav Udovenko <vladyslavu@uia.no>
2025-05-06 12:17:28 +0200

    Add fonts and dicrectory for locales

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Text, View } from 'react-native';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Regular': require('../assets/fonts/Inter-Regular.otf'),
+    'Inter-SemiBold': require('../assets/fonts/Inter-SemiBold.otf'),
+  });
+
+  if (!fontsLoaded) {
+    // Optional: Add your own splash or fallback here
+    return <View><Text>Loading...</Text></View>;
+  }
+
   return <Stack />;
 }
